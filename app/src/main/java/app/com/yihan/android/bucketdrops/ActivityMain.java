fb36242020c75bca4e341ad97cac2fc9d7a3cce2
Yi Han <yihanseattle@gmail.com>
2016-06-01 20:50:19 -0700

    Add listener for the Add Button

--- app/src/main/java/app/com/yihan/android/bucketdrops/ActivityMain.java
+++ app/src/main/java/app/com/yihan/android/bucketdrops/ActivityMain.java
@@ -4,19 +4,30 @@ import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Button mBtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this, "asdfsdafadfadsf", Toast.LENGTH_SHORT).show();
+            }
+        });
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
