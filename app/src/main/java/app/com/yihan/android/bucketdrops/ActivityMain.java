28cd4fa8c950c8dc9a2a9463cd37513dfccf6fc2
Yi Han <yihanseattle@gmail.com>
2016-06-03 12:35:08 -0700

    45 Add Menu in XML

--- app/src/main/java/app/com/yihan/android/bucketdrops/ActivityMain.java
+++ app/src/main/java/app/com/yihan/android/bucketdrops/ActivityMain.java
@@ -7,9 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -112,6 +115,23 @@ public class ActivityMain extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mResults.removeChangeListener(mChangeListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_add:
+                Toast.makeText(ActivityMain.this, "Add was clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void showDialogAdd() {
